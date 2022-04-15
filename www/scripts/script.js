ceec99e5428d451228b8a9f44f42f11869ba975c
Kirill Demidov <kademidov@gmail.com>
2022-04-15 18:20:46 +0300

    подключен jQuery. Открытие/закрытие меню на мобильное версии

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,21 @@
-console.log('script work!');
+$(document).ready(function(){
+
+  let isOpen = false;
+
+  $('.j-burger').on('click', function(){
+
+    $('.j-menu').slideToggle();
+
+    // if (isOpen) {
+    //   $('.j-menu').slideUp();
+    //   isOpen = false;
+    // } else {
+    //   $('.j-menu').slideDown();
+    //   isOpen = true;
+    // }
+
+  });
+
+});
+
+
