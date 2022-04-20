0a7f7b239e06c8a884a72af1c018ec9322eea27c
Kirill Demidov <kademidov@gmail.com>
2022-04-20 16:22:06 +0300

    табы в контактах и фильтрация работ

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -16,6 +16,49 @@ $(document).ready(function(){
 
   });
 
+
+  // Табы в контактах
+  $('.j-tabs-link').on('click', function(event){
+    event.preventDefault();
+
+    $('.j-tabs-link').removeClass('active');
+    $(this).addClass('active');
+
+    let index = $(this).index('.j-tabs-link');
+
+    $('.j-contacts-item').removeClass('active');
+    $('.j-contacts-item').eq(index).addClass('active');
+
+  });
+
+
+  // Фильтр в наших работах
+  $('.j-works-link').on('click', function(event){
+    event.preventDefault();
+
+    let filterData = $(this).data('filter');
+
+    console.log(filterData);
+
+    if (filterData === 'all') {
+      $('.works-item').show();
+      return;
+    }
+
+    $('.works-item').each(function(){
+
+      let typeData = $(this).data('type');
+
+      if (filterData === typeData) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+
+    });
+  });
+
+
 });
 
 
